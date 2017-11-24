c614fa0a118d93104bbd4f73538cd2a097169b6d
Alasdair Nicol <alasdair@thenicols.net>
2017-11-24 21:39:13 +0000

    Tests for Christmas 2017

--- tests.py
+++ tests.py
@@ -61,6 +61,86 @@ def test_chrismas_2016(regular_collection_day, input_date, collection_date, coll
     assert collection.types == collection_types
 
 
+@pytest.mark.parametrize("regular_collection_day,input_date,collection_date,collection_types", [
+    ["MONDAY", date(2017, 12, 25), date(2017, 12, 27), (RUBBISH,)],
+    ["MONDAY", date(2017, 12, 26), date(2017, 12, 27), (RUBBISH,)],
+    ["MONDAY", date(2017, 12, 27), date(2017, 12, 27), (RUBBISH,)],
+    ["MONDAY", date(2017, 12, 28), date(2018, 1, 3), (RECYCLING,)],
+
+    ["MONDAY", date(2018, 1, 1), date(2018, 1, 3), (RECYCLING,)],
+    ["MONDAY", date(2018, 1, 2), date(2018, 1, 3), (RECYCLING,)],
+    ["MONDAY", date(2018, 1, 3), date(2018, 1, 3), (RECYCLING,)],
+    ["MONDAY", date(2018, 1, 4), date(2018, 1, 9), (RUBBISH,)],
+
+    ["MONDAY", date(2018, 1, 8), date(2018, 1, 9), (RUBBISH,)],
+    ["MONDAY", date(2018, 1, 9), date(2018, 1, 9), (RUBBISH,)],
+    ["MONDAY", date(2018, 1, 10), date(2018, 1, 15), (RECYCLING, GARDEN)],  # back to normal
+
+    ["TUESDAY", date(2017, 12, 26), date(2017, 12, 28), (RUBBISH,)],
+    ["TUESDAY", date(2017, 12, 27), date(2017, 12, 28), (RUBBISH,)],
+    ["TUESDAY", date(2017, 12, 28), date(2017, 12, 28), (RUBBISH,)],
+    ["TUESDAY", date(2017, 12, 29), date(2018, 1, 4), (RECYCLING,)],
+
+    ["TUESDAY", date(2018, 1, 2), date(2018, 1, 4), (RECYCLING,)],
+    ["TUESDAY", date(2018, 1, 3), date(2018, 1, 4), (RECYCLING,)],
+    ["TUESDAY", date(2018, 1, 4), date(2018, 1, 4), (RECYCLING,)],
+    ["TUESDAY", date(2018, 1, 5), date(2018, 1, 10), (RUBBISH,)],
+
+    ["TUESDAY", date(2018, 1, 9), date(2018, 1, 10), (RUBBISH,)],
+    ["TUESDAY", date(2018, 1, 10), date(2018, 1, 10), (RUBBISH,)],
+    ["TUESDAY", date(2018, 1, 11), date(2018, 1, 16), (RECYCLING, GARDEN)],  # back to normal
+
+    ["WEDNESDAY", date(2017, 12, 27), date(2017, 12, 29), (RUBBISH,)],
+    ["WEDNESDAY", date(2017, 12, 28), date(2017, 12, 29), (RUBBISH,)],
+    ["WEDNESDAY", date(2017, 12, 29), date(2017, 12, 29), (RUBBISH,)],
+    ["WEDNESDAY", date(2017, 12, 30), date(2018, 1, 5), (RECYCLING,)],
+
+    ["WEDNESDAY", date(2018, 1, 3), date(2018, 1, 5), (RECYCLING,)],
+    ["WEDNESDAY", date(2018, 1, 4), date(2018, 1, 5), (RECYCLING,)],
+    ["WEDNESDAY", date(2018, 1, 5), date(2018, 1, 5), (RECYCLING,)],
+    ["WEDNESDAY", date(2018, 1, 6), date(2018, 1, 11), (RUBBISH,)],
+
+    ["WEDNESDAY", date(2018, 1, 10), date(2018, 1, 11), (RUBBISH,)],
+    ["WEDNESDAY", date(2018, 1, 11), date(2018, 1, 11), (RUBBISH,)],
+    ["WEDNESDAY", date(2018, 1, 12), date(2018, 1, 17), (RECYCLING, GARDEN)],  # back to normal
+
+    ["THURSDAY", date(2017, 12, 28), date(2017, 12, 30), (RUBBISH,)],
+    ["THURSDAY", date(2017, 12, 29), date(2017, 12, 30), (RUBBISH,)],
+    ["THURSDAY", date(2017, 12, 30), date(2017, 12, 30), (RUBBISH,)],
+    ["THURSDAY", date(2017, 12, 31), date(2018, 1, 6), (RECYCLING,)],
+
+    ["THURSDAY", date(2018, 1, 4), date(2018, 1, 6), (RECYCLING,)],
+    ["THURSDAY", date(2018, 1, 5), date(2018, 1, 6), (RECYCLING,)],
+    ["THURSDAY", date(2018, 1, 6), date(2018, 1, 6), (RECYCLING,)],
+    ["THURSDAY", date(2018, 1, 7), date(2018, 1, 12), (RUBBISH,)],
+
+    ["THURSDAY", date(2018, 1, 11), date(2018, 1, 12), (RUBBISH,)],
+    ["THURSDAY", date(2018, 1, 12), date(2018, 1, 12), (RUBBISH,)],
+    ["THURSDAY", date(2018, 1, 13), date(2018, 1, 18), (RECYCLING, GARDEN)],  # back to normal
+
+    ["FRIDAY", date(2017, 12, 29), date(2018, 1, 2), (RUBBISH,)],
+    ["FRIDAY", date(2017, 12, 30), date(2018, 1, 2), (RUBBISH,)],
+    ["FRIDAY", date(2017, 12, 31), date(2018, 1, 2), (RUBBISH,)],
+    ["FRIDAY", date(2018, 1, 1), date(2018, 1, 2), (RUBBISH,)],
+    ["FRIDAY", date(2018, 1, 2), date(2018, 1, 2), (RUBBISH,)],
+    ["FRIDAY", date(2018, 1, 3), date(2018, 1, 8), (RECYCLING,)],
+
+    ["FRIDAY", date(2018, 1, 5), date(2018, 1, 8), (RECYCLING,)],
+    ["FRIDAY", date(2018, 1, 6), date(2018, 1, 8), (RECYCLING,)],
+    ["FRIDAY", date(2018, 1, 7), date(2018, 1, 8), (RECYCLING,)],
+    ["FRIDAY", date(2018, 1, 8), date(2018, 1, 8), (RECYCLING,)],
+    ["FRIDAY", date(2018, 1, 9), date(2018, 1, 13), (RUBBISH,)],
+
+    ["FRIDAY", date(2018, 1, 12), date(2018, 1, 13), (RUBBISH,)],
+    ["FRIDAY", date(2018, 1, 13), date(2018, 1, 13), (RUBBISH,)],
+    ["FRIDAY", date(2018, 1, 14), date(2018, 1, 19), (RECYCLING, GARDEN)],  # back to normal
+])
+def test_christmas_2017(regular_collection_day, input_date, collection_date, collection_types):
+    collection = get_next_bin_collection(regular_collection_day, input_date)
+    assert collection.date == collection_date
+    assert collection.types == collection_types
+
+
 @pytest.mark.skip("Skipping while we add new dates")
 def test_no_collection_data():
     assert get_next_bin_collection("FRIDAY", date(2017, 10, 27)) is not None
