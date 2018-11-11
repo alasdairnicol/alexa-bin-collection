7e1652fcbd957f568847818a65d0495f79b2256f
Alasdair Nicol <alasdair@thenicols.net>
2018-11-11 22:29:04 +0000

    Remove test cases for dates before November 2018

--- tests.py
+++ tests.py
@@ -18,22 +18,6 @@ def test_default_input_date():
 
 
 @pytest.mark.parametrize("regular_collection_day,input_date,collection_date,collection_types", [
-    ["MONDAY", date(2017, 11, 1), date(2017, 11, 6), (RECYCLING, GARDEN)],
-    ["MONDAY", date(2017, 11, 6), date(2017, 11, 6), (RECYCLING, GARDEN)],
-    ["MONDAY", date(2017, 11, 7), date(2017, 11, 13), (RUBBISH,)],
-    ["MONDAY", date(2017, 11, 13), date(2017, 11, 13), (RUBBISH,)],
-    ["MONDAY", date(2017, 11, 14), date(2017, 11, 20), (RECYCLING, GARDEN)],
-    ["MONDAY", date(2018, 1, 15), date(2018, 1, 15), (RECYCLING, GARDEN,)],
-    ["MONDAY", date(2018, 1, 16), date(2018, 1, 22), (RUBBISH,)],
-
-    ["FRIDAY", date(2017, 11, 1), date(2017, 11, 3), (RUBBISH,)],
-    ["FRIDAY", date(2017, 11, 3), date(2017, 11, 3), (RUBBISH,)],
-    ["FRIDAY", date(2017, 11, 4), date(2017, 11, 10), (RECYCLING, GARDEN)],
-    ["FRIDAY", date(2017, 11, 10), date(2017, 11, 10), (RECYCLING, GARDEN)],
-    ["FRIDAY", date(2017, 11, 11), date(2017, 11, 17), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 19), date(2018, 1, 19), (RECYCLING, GARDEN,)],
-    ["FRIDAY", date(2018, 1, 20), date(2018, 1, 26), (RUBBISH,)],
-
     ["MONDAY", date(2018, 11, 1), date(2018, 11, 5), (RECYCLING, GARDEN)],
     ["MONDAY", date(2018, 11, 5), date(2018, 11, 5), (RECYCLING, GARDEN)],
     ["MONDAY", date(2018, 11, 6), date(2018, 11, 12), (RUBBISH,)],
@@ -66,86 +50,6 @@ def test_default_input_date():
     ["FRIDAY", date(2019, 11, 9), date(2019, 11, 15), (RUBBISH,)],
 ])
 def test_get_next_bin_collection(regular_collection_day, input_date, collection_date, collection_types):
-    collection = get_next_bin_collection(regular_collection_day, input_date)
-    assert collection.date == collection_date
-    assert collection.types == collection_types
-
-
-@pytest.mark.parametrize("regular_collection_day,input_date,collection_date,collection_types", [
-    ["MONDAY", date(2017, 12, 25), date(2017, 12, 27), (RUBBISH,)],
-    ["MONDAY", date(2017, 12, 26), date(2017, 12, 27), (RUBBISH,)],
-    ["MONDAY", date(2017, 12, 27), date(2017, 12, 27), (RUBBISH,)],
-    ["MONDAY", date(2017, 12, 28), date(2018, 1, 3), (RECYCLING,)],
-
-    ["MONDAY", date(2018, 1, 1), date(2018, 1, 3), (RECYCLING,)],
-    ["MONDAY", date(2018, 1, 2), date(2018, 1, 3), (RECYCLING,)],
-    ["MONDAY", date(2018, 1, 3), date(2018, 1, 3), (RECYCLING,)],
-    ["MONDAY", date(2018, 1, 4), date(2018, 1, 9), (RUBBISH,)],
-
-    ["MONDAY", date(2018, 1, 8), date(2018, 1, 9), (RUBBISH,)],
-    ["MONDAY", date(2018, 1, 9), date(2018, 1, 9), (RUBBISH,)],
-    ["MONDAY", date(2018, 1, 10), date(2018, 1, 15), (RECYCLING, GARDEN)],  # back to normal
-
-    ["TUESDAY", date(2017, 12, 26), date(2017, 12, 28), (RUBBISH,)],
-    ["TUESDAY", date(2017, 12, 27), date(2017, 12, 28), (RUBBISH,)],
-    ["TUESDAY", date(2017, 12, 28), date(2017, 12, 28), (RUBBISH,)],
-    ["TUESDAY", date(2017, 12, 29), date(2018, 1, 4), (RECYCLING,)],
-
-    ["TUESDAY", date(2018, 1, 2), date(2018, 1, 4), (RECYCLING,)],
-    ["TUESDAY", date(2018, 1, 3), date(2018, 1, 4), (RECYCLING,)],
-    ["TUESDAY", date(2018, 1, 4), date(2018, 1, 4), (RECYCLING,)],
-    ["TUESDAY", date(2018, 1, 5), date(2018, 1, 10), (RUBBISH,)],
-
-    ["TUESDAY", date(2018, 1, 9), date(2018, 1, 10), (RUBBISH,)],
-    ["TUESDAY", date(2018, 1, 10), date(2018, 1, 10), (RUBBISH,)],
-    ["TUESDAY", date(2018, 1, 11), date(2018, 1, 16), (RECYCLING, GARDEN)],  # back to normal
-
-    ["WEDNESDAY", date(2017, 12, 27), date(2017, 12, 29), (RUBBISH,)],
-    ["WEDNESDAY", date(2017, 12, 28), date(2017, 12, 29), (RUBBISH,)],
-    ["WEDNESDAY", date(2017, 12, 29), date(2017, 12, 29), (RUBBISH,)],
-    ["WEDNESDAY", date(2017, 12, 30), date(2018, 1, 5), (RECYCLING,)],
-
-    ["WEDNESDAY", date(2018, 1, 3), date(2018, 1, 5), (RECYCLING,)],
-    ["WEDNESDAY", date(2018, 1, 4), date(2018, 1, 5), (RECYCLING,)],
-    ["WEDNESDAY", date(2018, 1, 5), date(2018, 1, 5), (RECYCLING,)],
-    ["WEDNESDAY", date(2018, 1, 6), date(2018, 1, 11), (RUBBISH,)],
-
-    ["WEDNESDAY", date(2018, 1, 10), date(2018, 1, 11), (RUBBISH,)],
-    ["WEDNESDAY", date(2018, 1, 11), date(2018, 1, 11), (RUBBISH,)],
-    ["WEDNESDAY", date(2018, 1, 12), date(2018, 1, 17), (RECYCLING, GARDEN)],  # back to normal
-
-    ["THURSDAY", date(2017, 12, 28), date(2017, 12, 30), (RUBBISH,)],
-    ["THURSDAY", date(2017, 12, 29), date(2017, 12, 30), (RUBBISH,)],
-    ["THURSDAY", date(2017, 12, 30), date(2017, 12, 30), (RUBBISH,)],
-    ["THURSDAY", date(2017, 12, 31), date(2018, 1, 6), (RECYCLING,)],
-
-    ["THURSDAY", date(2018, 1, 4), date(2018, 1, 6), (RECYCLING,)],
-    ["THURSDAY", date(2018, 1, 5), date(2018, 1, 6), (RECYCLING,)],
-    ["THURSDAY", date(2018, 1, 6), date(2018, 1, 6), (RECYCLING,)],
-    ["THURSDAY", date(2018, 1, 7), date(2018, 1, 12), (RUBBISH,)],
-
-    ["THURSDAY", date(2018, 1, 11), date(2018, 1, 12), (RUBBISH,)],
-    ["THURSDAY", date(2018, 1, 12), date(2018, 1, 12), (RUBBISH,)],
-    ["THURSDAY", date(2018, 1, 13), date(2018, 1, 18), (RECYCLING, GARDEN)],  # back to normal
-
-    ["FRIDAY", date(2017, 12, 29), date(2018, 1, 2), (RUBBISH,)],
-    ["FRIDAY", date(2017, 12, 30), date(2018, 1, 2), (RUBBISH,)],
-    ["FRIDAY", date(2017, 12, 31), date(2018, 1, 2), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 1), date(2018, 1, 2), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 2), date(2018, 1, 2), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 3), date(2018, 1, 8), (RECYCLING,)],
-
-    ["FRIDAY", date(2018, 1, 5), date(2018, 1, 8), (RECYCLING,)],
-    ["FRIDAY", date(2018, 1, 6), date(2018, 1, 8), (RECYCLING,)],
-    ["FRIDAY", date(2018, 1, 7), date(2018, 1, 8), (RECYCLING,)],
-    ["FRIDAY", date(2018, 1, 8), date(2018, 1, 8), (RECYCLING,)],
-    ["FRIDAY", date(2018, 1, 9), date(2018, 1, 13), (RUBBISH,)],
-
-    ["FRIDAY", date(2018, 1, 12), date(2018, 1, 13), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 13), date(2018, 1, 13), (RUBBISH,)],
-    ["FRIDAY", date(2018, 1, 14), date(2018, 1, 19), (RECYCLING, GARDEN)],  # back to normal
-])
-def test_christmas_2017(regular_collection_day, input_date, collection_date, collection_types):
     collection = get_next_bin_collection(regular_collection_day, input_date)
     assert collection.date == collection_date
     assert collection.types == collection_types
