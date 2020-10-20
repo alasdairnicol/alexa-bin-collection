d6eb0e2cb957eaf7111184f6e15126c048c358ec
Alasdair Nicol <alasdair@thenicols.net>
2020-10-20 22:48:44 +0100

    Format with black

--- bins.py
+++ bins.py
@@ -2,30 +2,30 @@
 from __future__ import print_function
 from datetime import date, timedelta
 
-RECYCLING = 'recycling'
-RUBBISH = 'rubbish'
-GARDEN = 'garden'
+RECYCLING = "recycling"
+RUBBISH = "rubbish"
+GARDEN = "garden"
 
 
 WEEKDAYS = {
-    'MONDAY': 1,
-    'TUESDAY': 2,
-    'WEDNESDAY': 3,
-    'THURSDAY': 4,
-    'FRIDAY': 5,
-    'SATURDAY': 6,
-    'SUNDAY': 7,
+    "MONDAY": 1,
+    "TUESDAY": 2,
+    "WEDNESDAY": 3,
+    "THURSDAY": 4,
+    "FRIDAY": 5,
+    "SATURDAY": 6,
+    "SUNDAY": 7,
 }
 
 
-class CollectionDate():
+class CollectionDate:
     def __init__(self, collection_date, collection_types):
         self.date = collection_date
         self.types = collection_types
 
     @staticmethod
     def _suffix(d):
-        return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+        return "th" if 11 <= d <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(d % 10, "th")
 
     @property
     def friendly_date(self):
@@ -34,9 +34,9 @@ class CollectionDate():
         elif (self.date - date.today()).days == 1:
             return "tomorrow"
         else:
-            weekday = self.date.strftime('%A')
+            weekday = self.date.strftime("%A")
             day = "%s%s" % (self.date.day, self._suffix(self.date.day))
-            month = self.date.strftime('%B')
+            month = self.date.strftime("%B")
             return "on %s the %s of %s" % (weekday, day, month)
 
     def __str__(self):
@@ -49,13 +49,11 @@ EXCEPTIONS = {
     # date(2020, 12, 23): CollectionDate(date(2020, 12, 23), (RUBBISH,)),  # Regular day
     # date(2020, 12, 24): CollectionDate(date(2020, 12, 24), (RUBBISH,)),  # Regular day
     date(2020, 12, 25): CollectionDate(date(2020, 12, 28), (RUBBISH,)),
-
     date(2020, 12, 28): CollectionDate(date(2020, 12, 29), (RECYCLING,)),
     date(2020, 12, 29): CollectionDate(date(2020, 12, 30), (RECYCLING,)),
     date(2020, 12, 30): CollectionDate(date(2020, 12, 31), (RECYCLING,)),
     date(2020, 12, 31): CollectionDate(date(2021, 1, 2), (RECYCLING,)),
     date(2021, 1, 1): CollectionDate(date(2021, 1, 4), (RECYCLING,)),
-
     date(2021, 1, 4): CollectionDate(date(2021, 1, 5), (RUBBISH,)),
     date(2021, 1, 5): CollectionDate(date(2021, 1, 6), (RUBBISH,)),
     date(2021, 1, 6): CollectionDate(date(2021, 1, 7), (RUBBISH,)),
@@ -97,7 +95,7 @@ def get_next_bin_collection(regular_collection_day, input_date=None):
             return collection
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     collections = create_collections()
     for c in collections:
         print(c)
--- tests.py
+++ tests.py
@@ -17,106 +17,121 @@ def test_default_input_date():
         assert collection.types == (RECYCLING, GARDEN)
 
 
-@pytest.mark.parametrize("regular_collection_day,input_date,collection_date,collection_types", [
-    ["MONDAY", date(2020, 11, 1), date(2020, 11, 2), (RECYCLING, GARDEN)],
-    ["MONDAY", date(2020, 11, 2), date(2020, 11, 2), (RECYCLING, GARDEN)],
-    ["MONDAY", date(2020, 11, 3), date(2020, 11, 9), (RUBBISH,)],
-    ["MONDAY", date(2020, 11, 8), date(2020, 11, 9), (RUBBISH,)],
-    ["MONDAY", date(2020, 11, 9), date(2020, 11, 9), (RUBBISH,)],
-    ["MONDAY", date(2020, 11, 10), date(2020, 11, 16), (RECYCLING, GARDEN)],
-
-    ["FRIDAY", date(2020, 11, 1), date(2020, 11, 6), (RECYCLING, GARDEN)],
-    ["FRIDAY", date(2020, 11, 5), date(2020, 11, 6), (RECYCLING, GARDEN)],
-    ["FRIDAY", date(2020, 11, 6), date(2020, 11, 6), (RECYCLING, GARDEN)],
-    ["FRIDAY", date(2020, 11, 7), date(2020, 11, 13), (RUBBISH,)],
-    ["FRIDAY", date(2020, 11, 12), date(2020, 11, 13), (RUBBISH,)],
-    ["FRIDAY", date(2020, 11, 13), date(2020, 11, 13), (RUBBISH,)],
-    ["FRIDAY", date(2020, 11, 14), date(2020, 11, 20),(RECYCLING, GARDEN)],
-])
-def test_get_next_bin_collection(regular_collection_day, input_date, collection_date, collection_types):
+@pytest.mark.parametrize(
+    "regular_collection_day,input_date,collection_date,collection_types",
+    [
+        ["MONDAY", date(2020, 11, 1), date(2020, 11, 2), (RECYCLING, GARDEN)],
+        ["MONDAY", date(2020, 11, 2), date(2020, 11, 2), (RECYCLING, GARDEN)],
+        ["MONDAY", date(2020, 11, 3), date(2020, 11, 9), (RUBBISH,)],
+        ["MONDAY", date(2020, 11, 8), date(2020, 11, 9), (RUBBISH,)],
+        ["MONDAY", date(2020, 11, 9), date(2020, 11, 9), (RUBBISH,)],
+        ["MONDAY", date(2020, 11, 10), date(2020, 11, 16), (RECYCLING, GARDEN)],
+        ["FRIDAY", date(2020, 11, 1), date(2020, 11, 6), (RECYCLING, GARDEN)],
+        ["FRIDAY", date(2020, 11, 5), date(2020, 11, 6), (RECYCLING, GARDEN)],
+        ["FRIDAY", date(2020, 11, 6), date(2020, 11, 6), (RECYCLING, GARDEN)],
+        ["FRIDAY", date(2020, 11, 7), date(2020, 11, 13), (RUBBISH,)],
+        ["FRIDAY", date(2020, 11, 12), date(2020, 11, 13), (RUBBISH,)],
+        ["FRIDAY", date(2020, 11, 13), date(2020, 11, 13), (RUBBISH,)],
+        ["FRIDAY", date(2020, 11, 14), date(2020, 11, 20), (RECYCLING, GARDEN)],
+    ],
+)
+def test_get_next_bin_collection(
+    regular_collection_day, input_date, collection_date, collection_types
+):
     collection = get_next_bin_collection(regular_collection_day, input_date)
     assert collection.date == collection_date
     assert collection.types == collection_types
 
 
-@pytest.mark.parametrize("regular_collection_day,input_date,collection_date,collection_types", [
-    ["MONDAY", date(2020, 12, 20), date(2020, 12, 21), (RUBBISH,)],
-    ["MONDAY", date(2020, 12, 21), date(2020, 12, 21), (RUBBISH,)],
-    ["MONDAY", date(2020, 12, 22), date(2020, 12, 29), (RECYCLING,)],
-
-    ["MONDAY", date(2020, 12, 27), date(2020, 12, 29), (RECYCLING,)],
-    ["MONDAY", date(2020, 12, 28), date(2020, 12, 29), (RECYCLING,)],
-    ["MONDAY", date(2020, 12, 29), date(2020, 12, 29), (RECYCLING,)],
-    ["MONDAY", date(2020, 12, 30), date(2021, 1, 5), (RUBBISH,)],
-
-    ["MONDAY", date(2021, 1, 3), date(2021, 1, 5), (RUBBISH,)],
-    ["MONDAY", date(2021, 1, 4), date(2021, 1, 5), (RUBBISH,)],
-    ["MONDAY", date(2021, 1, 5), date(2021, 1, 5), (RUBBISH,)],
-    ["MONDAY", date(2021, 1, 6), date(2021, 1, 11), (RECYCLING, GARDEN)],  # back to normal
-
-    ["TUESDAY", date(2020, 12, 21), date(2020, 12, 22), (RUBBISH,)],
-    ["TUESDAY", date(2020, 12, 22), date(2020, 12, 22), (RUBBISH,)],
-    ["TUESDAY", date(2020, 12, 23), date(2020, 12, 30), (RECYCLING,)],
-
-    ["TUESDAY", date(2020, 12, 28), date(2020, 12, 30), (RECYCLING,)],
-    ["TUESDAY", date(2020, 12, 29), date(2020, 12, 30), (RECYCLING,)],
-    ["TUESDAY", date(2020, 12, 30), date(2020, 12, 30), (RECYCLING,)],
-    ["TUESDAY", date(2020, 12, 31), date(2021, 1, 6), (RUBBISH,)],
-
-    ["TUESDAY", date(2021, 1, 4), date(2021, 1, 6), (RUBBISH,)],
-    ["TUESDAY", date(2021, 1, 5), date(2021, 1, 6), (RUBBISH,)],
-    ["TUESDAY", date(2021, 1, 6), date(2021, 1, 6), (RUBBISH,)],
-    ["TUESDAY", date(2021, 1, 7), date(2021, 1, 12), (RECYCLING, GARDEN)],  # back to normal
-
-    ["WEDNESDAY", date(2020, 12, 22), date(2020, 12, 23), (RUBBISH,)],
-    ["WEDNESDAY", date(2020, 12, 23), date(2020, 12, 23), (RUBBISH,)],
-    ["WEDNESDAY", date(2020, 12, 24), date(2020, 12, 31), (RECYCLING,)],
-
-    ["WEDNESDAY", date(2020, 12, 29), date(2020, 12, 31), (RECYCLING,)],
-    ["WEDNESDAY", date(2020, 12, 30), date(2020, 12, 31), (RECYCLING,)],
-    ["WEDNESDAY", date(2020, 12, 31), date(2020, 12, 31), (RECYCLING,)],
-    ["WEDNESDAY", date(2021, 1, 1), date(2021, 1, 7), (RUBBISH,)],
-
-    ["WEDNESDAY", date(2021, 1, 5), date(2021, 1, 7), (RUBBISH,)],
-    ["WEDNESDAY", date(2021, 1, 6), date(2021, 1, 7), (RUBBISH,)],
-    ["WEDNESDAY", date(2021, 1, 7), date(2021, 1, 7), (RUBBISH,)],
-    ["WEDNESDAY", date(2021, 1, 8), date(2021, 1, 13), (RECYCLING, GARDEN)],
-
-    ["THURSDAY", date(2020, 12, 23), date(2020, 12, 24), (RUBBISH,)],
-    ["THURSDAY", date(2020, 12, 24), date(2020, 12, 24), (RUBBISH,)],
-    ["THURSDAY", date(2020, 12, 25), date(2021, 1, 2), (RECYCLING,)],
-
-    ["THURSDAY", date(2020, 12, 30), date(2021, 1, 2), (RECYCLING,)],
-    ["THURSDAY", date(2020, 12, 31), date(2021, 1, 2), (RECYCLING,)],
-    ["THURSDAY", date(2021, 1, 1), date(2021, 1, 2), (RECYCLING,)],
-    ["THURSDAY", date(2021, 1, 2), date(2021, 1, 2), (RECYCLING,)],
-    ["THURSDAY", date(2021, 1, 3), date(2021, 1, 8), (RUBBISH,)],
-
-    ["THURSDAY", date(2021, 1, 6), date(2021, 1, 8), (RUBBISH,)],
-    ["THURSDAY", date(2021, 1, 7), date(2021, 1, 8), (RUBBISH,)],
-    ["THURSDAY", date(2021, 1, 8), date(2021, 1, 8), (RUBBISH,)],
-    ["THURSDAY", date(2021, 1, 9), date(2021, 1, 14), (RECYCLING, GARDEN)],  # back to normal
-
-    ["FRIDAY", date(2020, 12, 24), date(2020, 12, 28), (RUBBISH,)],
-    ["FRIDAY", date(2020, 12, 25), date(2020, 12, 28), (RUBBISH,)],
-    ["FRIDAY", date(2020, 12, 26), date(2020, 12, 28), (RUBBISH,)],
-    ["FRIDAY", date(2020, 12, 27), date(2020, 12, 28), (RUBBISH,)],
-    ["FRIDAY", date(2020, 12, 28), date(2020, 12, 28), (RUBBISH,)],
-    ["FRIDAY", date(2020, 12, 29), date(2021, 1, 4), (RECYCLING,)],
-
-    ["FRIDAY", date(2020, 12, 31), date(2021, 1, 4), (RECYCLING,)],
-    ["FRIDAY", date(2021, 1, 1), date(2021, 1, 4), (RECYCLING,)],
-    ["FRIDAY", date(2021, 1, 2), date(2021, 1, 4), (RECYCLING,)],
-    ["FRIDAY", date(2021, 1, 3), date(2021, 1, 4), (RECYCLING,)],
-    ["FRIDAY", date(2021, 1, 4), date(2021, 1, 4), (RECYCLING,)],
-    ["FRIDAY", date(2021, 1, 5), date(2021, 1, 9), (RUBBISH,)],
-
-    ["FRIDAY", date(2021, 1, 7), date(2021, 1, 9), (RUBBISH,)],
-    ["FRIDAY", date(2021, 1, 8), date(2021, 1, 9), (RUBBISH,)],
-    ["FRIDAY", date(2021, 1, 9), date(2021, 1, 9), (RUBBISH,)],
-    ["FRIDAY", date(2021, 1, 10), date(2021, 1, 15), (RECYCLING, GARDEN)],  # back to normal
-])
-def test_christmas_2020(regular_collection_day, input_date, collection_date, collection_types):
+@pytest.mark.parametrize(
+    "regular_collection_day,input_date,collection_date,collection_types",
+    [
+        ["MONDAY", date(2020, 12, 20), date(2020, 12, 21), (RUBBISH,)],
+        ["MONDAY", date(2020, 12, 21), date(2020, 12, 21), (RUBBISH,)],
+        ["MONDAY", date(2020, 12, 22), date(2020, 12, 29), (RECYCLING,)],
+        ["MONDAY", date(2020, 12, 27), date(2020, 12, 29), (RECYCLING,)],
+        ["MONDAY", date(2020, 12, 28), date(2020, 12, 29), (RECYCLING,)],
+        ["MONDAY", date(2020, 12, 29), date(2020, 12, 29), (RECYCLING,)],
+        ["MONDAY", date(2020, 12, 30), date(2021, 1, 5), (RUBBISH,)],
+        ["MONDAY", date(2021, 1, 3), date(2021, 1, 5), (RUBBISH,)],
+        ["MONDAY", date(2021, 1, 4), date(2021, 1, 5), (RUBBISH,)],
+        ["MONDAY", date(2021, 1, 5), date(2021, 1, 5), (RUBBISH,)],
+        [
+            "MONDAY",
+            date(2021, 1, 6),
+            date(2021, 1, 11),
+            (RECYCLING, GARDEN),
+        ],  # back to normal
+        ["TUESDAY", date(2020, 12, 21), date(2020, 12, 22), (RUBBISH,)],
+        ["TUESDAY", date(2020, 12, 22), date(2020, 12, 22), (RUBBISH,)],
+        ["TUESDAY", date(2020, 12, 23), date(2020, 12, 30), (RECYCLING,)],
+        ["TUESDAY", date(2020, 12, 28), date(2020, 12, 30), (RECYCLING,)],
+        ["TUESDAY", date(2020, 12, 29), date(2020, 12, 30), (RECYCLING,)],
+        ["TUESDAY", date(2020, 12, 30), date(2020, 12, 30), (RECYCLING,)],
+        ["TUESDAY", date(2020, 12, 31), date(2021, 1, 6), (RUBBISH,)],
+        ["TUESDAY", date(2021, 1, 4), date(2021, 1, 6), (RUBBISH,)],
+        ["TUESDAY", date(2021, 1, 5), date(2021, 1, 6), (RUBBISH,)],
+        ["TUESDAY", date(2021, 1, 6), date(2021, 1, 6), (RUBBISH,)],
+        [
+            "TUESDAY",
+            date(2021, 1, 7),
+            date(2021, 1, 12),
+            (RECYCLING, GARDEN),
+        ],  # back to normal
+        ["WEDNESDAY", date(2020, 12, 22), date(2020, 12, 23), (RUBBISH,)],
+        ["WEDNESDAY", date(2020, 12, 23), date(2020, 12, 23), (RUBBISH,)],
+        ["WEDNESDAY", date(2020, 12, 24), date(2020, 12, 31), (RECYCLING,)],
+        ["WEDNESDAY", date(2020, 12, 29), date(2020, 12, 31), (RECYCLING,)],
+        ["WEDNESDAY", date(2020, 12, 30), date(2020, 12, 31), (RECYCLING,)],
+        ["WEDNESDAY", date(2020, 12, 31), date(2020, 12, 31), (RECYCLING,)],
+        ["WEDNESDAY", date(2021, 1, 1), date(2021, 1, 7), (RUBBISH,)],
+        ["WEDNESDAY", date(2021, 1, 5), date(2021, 1, 7), (RUBBISH,)],
+        ["WEDNESDAY", date(2021, 1, 6), date(2021, 1, 7), (RUBBISH,)],
+        ["WEDNESDAY", date(2021, 1, 7), date(2021, 1, 7), (RUBBISH,)],
+        ["WEDNESDAY", date(2021, 1, 8), date(2021, 1, 13), (RECYCLING, GARDEN)],
+        ["THURSDAY", date(2020, 12, 23), date(2020, 12, 24), (RUBBISH,)],
+        ["THURSDAY", date(2020, 12, 24), date(2020, 12, 24), (RUBBISH,)],
+        ["THURSDAY", date(2020, 12, 25), date(2021, 1, 2), (RECYCLING,)],
+        ["THURSDAY", date(2020, 12, 30), date(2021, 1, 2), (RECYCLING,)],
+        ["THURSDAY", date(2020, 12, 31), date(2021, 1, 2), (RECYCLING,)],
+        ["THURSDAY", date(2021, 1, 1), date(2021, 1, 2), (RECYCLING,)],
+        ["THURSDAY", date(2021, 1, 2), date(2021, 1, 2), (RECYCLING,)],
+        ["THURSDAY", date(2021, 1, 3), date(2021, 1, 8), (RUBBISH,)],
+        ["THURSDAY", date(2021, 1, 6), date(2021, 1, 8), (RUBBISH,)],
+        ["THURSDAY", date(2021, 1, 7), date(2021, 1, 8), (RUBBISH,)],
+        ["THURSDAY", date(2021, 1, 8), date(2021, 1, 8), (RUBBISH,)],
+        [
+            "THURSDAY",
+            date(2021, 1, 9),
+            date(2021, 1, 14),
+            (RECYCLING, GARDEN),
+        ],  # back to normal
+        ["FRIDAY", date(2020, 12, 24), date(2020, 12, 28), (RUBBISH,)],
+        ["FRIDAY", date(2020, 12, 25), date(2020, 12, 28), (RUBBISH,)],
+        ["FRIDAY", date(2020, 12, 26), date(2020, 12, 28), (RUBBISH,)],
+        ["FRIDAY", date(2020, 12, 27), date(2020, 12, 28), (RUBBISH,)],
+        ["FRIDAY", date(2020, 12, 28), date(2020, 12, 28), (RUBBISH,)],
+        ["FRIDAY", date(2020, 12, 29), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2020, 12, 31), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2021, 1, 1), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2021, 1, 2), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2021, 1, 3), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2021, 1, 4), date(2021, 1, 4), (RECYCLING,)],
+        ["FRIDAY", date(2021, 1, 5), date(2021, 1, 9), (RUBBISH,)],
+        ["FRIDAY", date(2021, 1, 7), date(2021, 1, 9), (RUBBISH,)],
+        ["FRIDAY", date(2021, 1, 8), date(2021, 1, 9), (RUBBISH,)],
+        ["FRIDAY", date(2021, 1, 9), date(2021, 1, 9), (RUBBISH,)],
+        [
+            "FRIDAY",
+            date(2021, 1, 10),
+            date(2021, 1, 15),
+            (RECYCLING, GARDEN),
+        ],  # back to normal
+    ],
+)
+def test_christmas_2020(
+    regular_collection_day, input_date, collection_date, collection_types
+):
     collection = get_next_bin_collection(regular_collection_day, input_date)
     assert collection.date == collection_date
     assert collection.types == collection_types
@@ -140,11 +155,11 @@ def test_no_collection_data_too_early():
 
 def test_friendly_date():
     with freeze_time("2017-1-15"):
-        collection = CollectionDate(date(2017,1,15), [])
+        collection = CollectionDate(date(2017, 1, 15), [])
         assert collection.friendly_date == "today"
 
-        collection = CollectionDate(date(2017,1,16), [])
+        collection = CollectionDate(date(2017, 1, 16), [])
         assert collection.friendly_date == "tomorrow"
 
-        collection = CollectionDate(date(2017,1,17), [])
+        collection = CollectionDate(date(2017, 1, 17), [])
         assert collection.friendly_date == "on Tuesday the 17th of January"
